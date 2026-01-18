8f20a47cf54430a73d650ec5c257b0d3d0c05acc
jaredstemper <31346981+JaredStemper@users.noreply.github.com>
2026-01-18 08:02:08 -0500

    updated to ensure assets and relative root

--- resume/latex_build.sh
+++ resume/latex_build.sh
@@ -1,2 +1,21 @@
-docker build . --file resume/Dockerfile --tag latexBuild
-docker run --rm -v $(pwd)/resume:/input -v $(pwd)/resume/build:/output latexBuild
+#!/usr/bin/env bash
+set -euo pipefail
+
+# point to relative repo root regardless of where script is called
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]:-$0}")" && pwd)"
+repo_root="$(cd "$script_dir/.." && pwd)"
+
+docker build "$repo_root" --file "$repo_root/resume/Dockerfile" --tag latexbuild
+
+resume_dir="$repo_root/resume"
+assets_dir="$repo_root/assets/resume"
+
+# output build to host
+mkdir -p "$resume_dir/build"
+docker run --rm \
+	-v "$resume_dir":/input \
+	-v "$resume_dir":/output \
+	-v "$assets_dir":/assets \
+	latexbuild
+
+echo "Build finished. Enjoy your new resume at $resume_dir/build and have just a heck of a day over there"
